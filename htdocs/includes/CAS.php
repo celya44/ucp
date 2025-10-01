6551d9eda4174c6e1eb6a398c79c1e7b5dacaa8a
Julien Baron <jbaron@celya.fr>
2025-10-01 18:01:35 +0200

    Utilisation de la librairie CAS présente dans le module

--- htdocs/includes/CAS.php
+++ htdocs/includes/CAS.php
@@ -16,7 +16,7 @@ if(!$PHPCASISINSTALLED){
 	exit(1);
 }
 // import phpCAS lib
-include_once('CAS/CAS.php');
+include_once(__DIR__ . '/../vendor/autoload.php');
 
 // Enable debugging
 //phpCAS::setDebug();
@@ -42,7 +42,7 @@ $ldap_parameter = $ucp->FreePBX->Config->get("UCPCASLDAPPARAM");
 
 // Initialize phpCAS Client
 $relMethod = new ReflectionMethod('phpCAS','client');
-if ($relMethod->getNumberOfParameters() === 6){
+if ($relMethod->getNumberOfParameters() >= 6){
     phpCAS::client(SAML_VERSION_1_1, $cas_host, $cas_port, $cas_context, $service_url);
 }else{
     phpCAS::client(SAML_VERSION_1_1, $cas_host, $cas_port, $cas_context);
