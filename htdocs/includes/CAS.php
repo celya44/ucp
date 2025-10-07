5ec1cc0347757d88434d5a9b0cfce7424724ea2b
Julien Baron <jbaron@celya.fr>
2025-10-07 15:01:03 +0200

    Supprime check librairie CAS existe

--- htdocs/includes/CAS.php
+++ htdocs/includes/CAS.php
@@ -3,18 +3,6 @@
  *  CAS auth +  SAML with attributes
  */
 
-$ps = explode(':',get_include_path());
-$PHPCASISINSTALLED = false;
-foreach( $ps as $p )
-{
-	if(file_exists($p.'/'.'CAS/CAS.php')){
-		$PHPCASISINSTALLED = true;
-	}
-}
-if(!$PHPCASISINSTALLED){
-    echo "<div>Erreur : la librairie php-cas n'est pas installée<br><br>Merci de lancer en ligne de commande :<br><pre>apt-get install php-cas</pre></div>";
-	exit(1);
-}
 // import phpCAS lib
 include_once(__DIR__ . '/../vendor/autoload.php');
 
